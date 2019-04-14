f6c2bf46c8f057b8df41a79781bd3ac1d4003d78
leonardo <lfreddo@inf.ufsm.br>
2019-04-14 14:17:02 -0300

    .

--- t2/t2.hs
+++ t2/t2.hs
@@ -10,7 +10,33 @@ type Circle (Point, Float)
 
 
 
+
+
+
+
+
+
+
 -- SVG Functions
+
+svgRect :: Rect -> String -> String
+svgRect ((x, y), w, h) style = printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
+
+svgCirc :: Circle -> String -> String
+svgCirc ((x, y), r) style = printf "<circle cx='%.3f' cy='.3f' />" 
+
+svgBegin :: Float -> Float -> String
+svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h
+
+svgEnd :: String
+svgEnd = "</svg>"
+
+
+
+genCase1 :: IO()
+genCase1 = writeFile "case1.svg" svgstrs
+    where
+        svgstrs = svgBegin w h
 
 main = do
     genCase1
