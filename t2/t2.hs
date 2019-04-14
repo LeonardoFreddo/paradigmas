9f75b0a570b25d5c69246f225e912317a4bd4384
leonardo <lfreddo@inf.ufsm.br>
2019-04-14 14:04:57 -0300

    modify t2.hs

--- t2/t2.hs
+++ t2/t2.hs
@@ -1,1 +1,19 @@
- 
+
+import Text.Printf
+
+type Point (Float, Float)
+-- x, y
+type Rect (Point, Float, Float)
+--point, width, height
+type Circle (Point, Float)
+--point, radius
+
+
+
+-- SVG Functions
+
+main = do
+    genCase1
+
+
+
