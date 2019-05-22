fac2486288a895c8a48f51c72db36030cb62021d
Leonardo Freddo <lfreddo@ncc3.inf.ufsm.br>
2019-05-22 15:03:28 -0300

    Update pratica2

---
+++ praticas/poo/pratica2/Professor.java
@@ -0,0 +1,14 @@
+package pratica2;
+
+public class Professor extends Pessoa{
+    private int siape;
+    private double salario;
+
+    public int getSiape(){
+      return this.siape;
+    }
+
+    public void setSiape(int siape){
+      this.siape = siape;
+    }
+}
