9bdffaa29bb9f1398f78d4be4afc34f7137a674a
LeonardoFreddo <lfreddo@inf.ufsm.br>
2019-05-22 23:22:03 -0300

    Update pratica3

--- praticas/poo/pratica3/Empresa/Main.java
+++ praticas/poo/pratica3/Empresa/Main.java
@@ -1,11 +1,11 @@
 package Empresa;
 
 class Main{
-  public static void main(String agrv[]){
-    Funcionario func1 = new Funcionario("Joao", 1234567, 1000.8);
-    Gerente gen1 = new Gerente("Lala", 1265789, 10000.23);
+  	public static void main(String agrv[]){
+		Funcionario func1 = new Funcionario("Joao", 1234567, 1000.8);
+    	Gerente gen1 = new Gerente("Lala", 1265789, 10000.23);
 
-    func1.mostraFuncionario();
-    gen1.mostraFuncionario();
+    	func1.mostraFuncionario();
+    	gen1.mostraFuncionario();
   }
 }
